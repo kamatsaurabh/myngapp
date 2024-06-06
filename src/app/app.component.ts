7c81d7c5959c3304a8013c7eabd57683276500f1
kamatsaurabh <saurabh.kamat@intelegencia.com>
2024-06-06 21:20:21 +0530

    devExtreme intalled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
+import config from 'devextreme/core/config';
+import { licenseKey } from './devextreme-license';
+import { DxButtonModule } from 'devextreme-angular';
+config({
+    licenseKey:licenseKey
+})
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule,DxButtonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
